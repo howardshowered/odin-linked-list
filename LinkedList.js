5d616db9d669f214eb750388541b9aa0da3afec1
Howard Chen <194877133+howardshowered@users.noreply.github.com>
2026-02-05 21:24:49 -0800

    intialized js files

---
+++ LinkedList.js
@@ -0,0 +1,22 @@
+import { Node } from "./Node.js";
+
+export class LinkedList {
+    constructor() {
+        this.head = null;
+        this.size = 0;
+    }
+
+    append(value) {
+        let newNode = Node(value);
+        if (!head)
+            head = newNode;
+        else {
+            let tempNode;
+            
+        }
+            
+
+    }
+
+
+}
