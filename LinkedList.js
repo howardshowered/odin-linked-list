ba52ceeca991ef5a155e59547b2a71d69f558ec6
Howard Chen <194877133+howardshowered@users.noreply.github.com>
2026-02-07 23:43:24 -0800

    added all required methods. insertAt method still needs work

--- LinkedList.js
+++ LinkedList.js
@@ -3,19 +3,171 @@ import { Node } from "./Node.js";
 export class LinkedList {
     constructor() {
         this.head = null;
+        this.next = null;
+        this.tail = null;
         this.size = 0;
     }
 
     append(value) {
-        let newNode = Node(value);
-        if (!head)
+        const newNode = new Node(value);
+        if (!this.head){
+            this.head = newNode;
+            this.tail = null;
+            this.size++;
+            return;
+        }
+        
+        let currentNode = this.head;
+        while(currentNode.next) {
+            currentNode = currentNode.next;
+        }
+        
+        currentNode.next = newNode;
+        this.tail = newNode;
+        this.size++;
+        
+    }
+
+    prepend( value ) {
+        const newNode = new Node(value);
+        if(!head){
             head = newNode;
-        else {
-            let tempNode = head;
-            while(!tempNode.next) {
-                tempNode = tempNode.next;
+            return;
+        }
+
+        let currentNode = this.head;
+        newNode.next = currentNode;
+        this.head = newNode;
+        this.size++;
+
+    } 
+
+    size () {
+        return this.size;
+    }
+
+    head() {
+        return this.head;
+    }
+
+    //@Todo add a tail memeber variable
+    tail() {
+        if(!head)
+            return undefined;
+        let currentNode = this.head;
+        while(currentNode.next)
+        {
+            currentNode = currentNode.next;
+        }
+
+        return currentNode;
+    }
+
+    at(index) {
+        if( index >= size) {
+            return undefined;
+        }
+
+        let currentNode = this.head;
+        for(let i = 0; i <  index; i++) {
+            currentNode = currentNode.next;
+        }
+
+        return currentNode;
+    }
+
+    pop() {
+        if(!this.head)
+            return undefined;
+
+        let currentNode = this.head;
+        let previousNode;
+        while(currentNode.next)
+        {
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+        }
+        previousNode.next = null;
+        this.size--;
+        if(this.size === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+    }
+
+    contains(value) {
+        if(!this.head)
+            return false;
+
+        let currentNode = this.head;
+        while(currentNode.next)
+        {
+            if(currentNode.value === value)
+                return true;
+            currentNode = currentNode.next;
+        }
+        return false;
+
+    }
+
+    findIndex(value) {
+        if(!this.head)
+            return -1;
+
+        let currentNode = this.head;
+        let currentIndex = 0;
+        while(currentNode.next)
+        {
+            if(currentNode.value === value)
+                return currentIndex;
+            currentNode = currentNode.next;
+            currentIndex++;
+        }
+        return -1;
+    
+    }
+
+    toString() {
+        if(!this.head)
+            return '';
+        let currentNode = this.head;
+        let result = '';
+        while(currentNode.next)
+        {
+
+            result += currentNode.value + ' -> ';
+
+            currentNode = currentNode.next;
+        }
+
+        if(currentNode.value === this.tail.value)
+            result += currentNode.value;
+
+        return result;
+
+    }
+
+
+    //@TODO.. extra credit but good exercise
+    insertAt(index, ...values) {
+        if( index < 0 || index >= size)
+            throw new RangeError(`The index must be valid`);
+        let currentNode = this.head;
+        let currentIndex = 0;
+        let previousNode;
+        while(currentNode.next)
+        {
+            if(currentIndex === index)
+            {
+                for( value in values)
+                {
+                    
+                }
+
             }
-            tempNode.next = newNode;
+            previousNode = currentNode;
+            currentNode = currentNode.next;
+            currentIndex++;
         }
 
     }
